a73e2af8e8c4a591192426a0ec90e4b678249b22
Md. Moshfequr Rahman <md.moshfequr@lbsbd.com>
2026-04-20 12:29:07 +0600

    firts commit

---
+++ Assets/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    // Smooth scroll
+    $(".nav-link, .btn").on("click", function (e) {
+        if (this.hash !== "") {
+            e.preventDefault();
+
+            const hash = this.hash;
+
+            $("html, body").animate({
+                scrollTop: $(hash).offset().top - 70
+            }, 600);
+        }
+    });
+
+});
